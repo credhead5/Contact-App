323624b8405a6939eabff2f7e0dfb274a16e0da5
christine <credhead89@gmail.com>
2020-01-20 20:29:17 -0500

    first commit

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,13 @@
+class Api::ContactsController < ApplicationController
+
+  def index
+    @contact = Contact.all
+    render "index.json.jb"
+  end
+
+  def show
+    @contact = Contact.find_by(id:params[:id])
+    render "show.json.jb"
+
+  end
+end
